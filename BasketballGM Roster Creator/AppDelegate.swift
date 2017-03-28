1328d0120cb21b4dc260c1b321643cbba3886ea0
Jeff Moran <Jmc113093@gmail.com>
2017-03-27 21:24:28 -0400

    Initial Xcode project

---
+++ BasketballGM Roster Creator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BasketballGM Roster Creator
+//
+//  Created by Jeff Moran on 3/27/17.
+//  Copyright © 2017 Jeff Moran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BasketballGM Roster Creator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BasketballGM Roster Creator
+//
+//  Created by Jeff Moran on 3/27/17.
+//  Copyright © 2017 Jeff Moran. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
